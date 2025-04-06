6df38b264b28c00757c2c60fd8b98950f2a240b1
AfridahKhan <afridahkhan@gmail.com>
2025-04-06 15:04:04 +0600

    Update SleepTracker.java

--- src/SleepTracker.java
+++ src/SleepTracker.java
@@ -9,17 +9,10 @@ class SleepTracker extends HealthFeature {
     }
 
     public void logData() {
-        writeToFile("sleep.csv", user.getName() + "," + hours);
+        FileManager.appendToFile("sleep.csv", user.getName() + "," + hours);
     }
 
     public String getFeedback() {
-        if (hours >= 7) return "You had a good night's sleep!";
-        return "Try to sleep at least 7 hours for better health.";
-    }
-
-    private void writeToFile(String filename, String data) {
-        try (FileWriter fw = new FileWriter(filename, true)) {
-            fw.write(data + "\n");
-        } catch (IOException e) { e.printStackTrace(); }
+        return hours >= 7 ? "You had a good night's sleep!" : "Try to sleep at least 7 hours.";
     }
 }
