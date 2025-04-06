1c25c4f0a103fba35823985b8ee2f499f19e04a6
AfridahKhan <afridahkhan@gmail.com>
2025-04-06 09:45:47 +0600

    Update StepTracker.java

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -9,18 +9,11 @@ class StepTracker extends HealthFeature {
     }
 
     public void logData() {
-        writeToFile("steps.csv", user.getName() + "," + steps);
+        FileManager.appendToFile("steps.csv", user.getName() + "," + steps);
     }
 
     public String getFeedback() {
-        if (steps >= 8000) return "Great job! You met your daily steps goal!";
-        return "Try to walk a bit more today.";
-    }
-
-    private void writeToFile(String filename, String data) {
-        try (FileWriter fw = new FileWriter(filename, true)) {
-            fw.write(data + "\n");
-        } catch (IOException e) { e.printStackTrace(); }
+        return steps >= 8000 ? "Great job! You met your daily steps goal!" : "Try to walk a bit more today.";
     }
 }
 
