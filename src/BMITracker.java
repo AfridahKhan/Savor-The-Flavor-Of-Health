28291bad8c17b8b10b75d61702c7e9c2b891316f
AfridahKhan <afridahkhan@gmail.com>
2025-04-06 15:02:32 +0600

    Update BMITracker.java

--- src/BMITracker.java
+++ src/BMITracker.java
@@ -7,19 +7,13 @@ class BMITracker extends HealthFeature {
 
     public void logData() {
         double bmi = user.getBMI();
-        writeToFile("bmi.csv", user.getName() + "," + bmi);
+        FileManager.appendToFile("bmi.csv", user.getName() + "," + bmi);
     }
 
     public String getFeedback() {
         double bmi = user.getBMI();
         if (bmi < 18.5) return "You're underweight. Consider a healthy diet plan.";
-        else if (bmi < 24.9) return "You're at a healthy weight. Great job!";
-        else return "Consider a fitness and nutrition plan to lower your BMI.";
-    }
-
-    private void writeToFile(String filename, String data) {
-        try (FileWriter fw = new FileWriter(filename, true)) {
-            fw.write(data + "\n");
-        } catch (IOException e) { e.printStackTrace(); }
+        else if (bmi < 24.9) return "You're at a healthy weight!";
+        else return "Consider a fitness and nutrition plan.";
     }
 }
